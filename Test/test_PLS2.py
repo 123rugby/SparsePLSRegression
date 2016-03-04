bd30e5a7095785ed05a1980622f86887018d4706
unknown <SimonCB765@gmail.com>
2016-03-04 14:13:03 +0000

    Added basic test for PLS2

---
+++ Test/test_PLS2.py
@@ -0,0 +1,27 @@
+import numpy
+import PLS.main
+from scipy import sparse
+import unittest
+
+class CompletionTests(unittest.TestCase):
+    """Tests checking whether the code doesn't crash while running PLS1."""
+
+    @classmethod
+    def setUpClass(self):
+        """Setup inputs needed for tests in the class."""
+
+        self.xSmall = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        self.ySmall = numpy.array([[1, 0], [0, 1], [1, 0]])
+
+    def test_pass_small(self):
+        PLS.main.pls(self.xSmall, self.ySmall)
+
+
+class CorrectnessTests(unittest.TestCase):
+    """Tests checking the correctness of the output while running PLS1."""
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
