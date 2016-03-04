f225dfed50d8171a4ccb177f36324b82acdb4508
unknown <SimonCB765@gmail.com>
2016-03-04 17:41:10 +0000

    Added empty SIMPLS test

---
+++ Test/test_simpls.py
@@ -0,0 +1,23 @@
+import numpy
+import PLS.simpls
+from scipy import sparse
+import unittest
+
+class CompletionTests(unittest.TestCase):
+    """Tests checking whether the SIMPLS code successfully returns."""
+
+    @classmethod
+    def setUpClass(self):
+        """Setup inputs needed for tests in the class."""
+
+        pass
+
+
+class CorrectnessTests(unittest.TestCase):
+    """Tests checking the correctness of the output."""
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
