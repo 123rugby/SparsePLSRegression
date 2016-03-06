41e9223e9c31da1ab285b9c0f7dfce90ce3ba8f7
unknown <SimonCB765@gmail.com>
2016-03-06 11:27:40 +0000

    Added function to count lines in a file

---
+++ PLS/line_counter.py
@@ -0,0 +1,24 @@
+import itertools
+
+
+def line_counter(fileLocation):
+    """Count the lines in a file.
+
+    :param fileLocation:    The location of the file.
+    :type fileLocation:     string
+
+    """
+
+    fid = open(fileLocation, 'rb')  # Open the file to read binary data.
+
+    # Setup for reading the file.
+    infiniteIterator = itertools.repeat(None)  # Create an iterator that returns None indefinitely.
+    takewhilePredicate = lambda x : x  # Will ensure that takewhile stops once the line returned is "False" (i.e. once it is an EOF).
+
+    # Create a generator that will repeat(edly) read a chunk of the file in until the line is "False" (i.e. once it is EOF).
+    readGen = itertools.takewhile(takewhilePredicate, (fid.raw.read(1024*1024) for _ in infiniteIterator))
+
+    lines = sum(buf.count(b'\n') for buf in readGen)  # Count occurrences of '\n' (and therefore number of lines) in each chunk of the file.
+    fid.close()
+
+    return lines
